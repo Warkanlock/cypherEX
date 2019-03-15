a50e038302ca732093c397a7dc48842d822d78ec
Ignacio <warkanlock@gmail.com>
2019-03-15 17:45:04 -0300

    First Code commit

---
+++ src/cypherEX-algo.py
@@ -0,0 +1,80 @@
+import time
+import random 
+import matplotlib.pyplot as plt
+import timeit
+start = time.time()
+
+#TESTING SEED
+import time
+year, month, day, hour, minute, secunde = time.strftime("%Y,%m,%d,%I,%M,%S").split(',')
+
+#GENERATE THE SEED AND TIME 
+#localtime = time.asctime( time.localtime(time.time()) )
+#print "seed form: ", localtime
+#seed = input("Enter the seed: ")
+
+seed = (int(year)/int(month)) + (int(month) + int(day) + int(hour) + int(minute)) ** int(secunde)
+peso = 60 #peso
+print "seed -> ", seed
+
+#INSERT THE CODE TO INPUT
+code = input("insert the code to encrypt -> ")
+
+random.seed(seed);
+
+key = random.random() #primary key
+key_constelacion = ((random.uniform(peso, seed))/(seed/key)) #constelation_key
+
+def generate_random_key_list(key, key_constelacion):
+	secret_key = []
+	for i in range(0,10):
+		data = (i)/(key)*((key-1)**key_constelacion**time.time()) + peso 
+		if(data>128):
+			data = data/4
+		secret_key.append(data)
+	return secret_key
+
+def generate_word(code):
+	word_complete = ""
+	for i in range(0,len(code)):
+		word_complete = word_complete + (code[i] + '.')
+	return word_complete.split('.')
+
+def encode(word, list_keys):
+	for i in range(0, len(word)-1):
+		word[i] = repr(list_keys[random.randint(0,9)])
+
+	word_encode = word
+
+	return word_encode
+
+def decode(word_encode):
+	output_word = []
+	for i in range(0, len(word_encode)-1):
+		output_word.append(str(unichr(int(float(word_encode[i])))))
+	return output_word
+
+#random values to the key
+testing_list = generate_random_key_list(key, key_constelacion)
+print testing_list
+
+#recive the word
+word_complete = generate_word(code)
+
+#write the encrypt word
+print word_complete
+
+#abstract the information
+word_encode = encode(word_complete, testing_list)
+print word_encode
+
+#To text
+print decode(word_encode)
+
+word_final = ''.join(decode(word_encode))
+print word_final 
+
+end = time.time()
+
+print "time required" 
+print(end-start)
